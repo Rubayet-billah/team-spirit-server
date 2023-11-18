1c1d29f3f75f3dff1ab5a74f4f3e1cfe09394531
Rubayet-billah <rubayetbillah909@gmail.com>
2023-11-19 03:57:33 +0600

    cors added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { connect } = require("mongoose");
 const router = require("./src/app/routes");
 
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
